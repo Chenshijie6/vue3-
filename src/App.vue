8d69ed2ffa51efda2140503e141e9a61257049c2
你的名字或昵称 <1584171644@qq.com>
2023-05-23 14:21:40 +0800

    scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
-
 </script>
 
 <template>
   <!-- <el-button type="primary">Primary</el-button> -->
   <RouterView></RouterView>
+  <div class="test">test scss</div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.test {
+  color: $priceColor;
+}
 </style>
