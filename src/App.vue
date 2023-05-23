87a6d1e57aa801fb251efb5ff6720782e76d94e4
你的名字或昵称 <1584171644@qq.com>
2023-05-23 14:37:31 +0800

    layout-静态结构搭建

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,8 @@
 <template>
   <!-- <el-button type="primary">Primary</el-button> -->
   <RouterView></RouterView>
-  <div class="test">test scss</div>
 </template>
 
 <style scoped lang="scss">
-.test {
-  color: $priceColor;
-}
+
 </style>
