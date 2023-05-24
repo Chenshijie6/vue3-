18b1599fe6e3f83775644f068cb49bd313560d86
你的名字或昵称 <1584171644@qq.com>
2023-05-24 13:37:53 +0800

    激活状态显示以及分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,6 +3,7 @@ import { getCategoryAPI } from "@/apis/category";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import { getBannerAPI } from "@/apis/home";
+import GoodsItem from "../Home/components/GoodsItem.vue";
 
 const categoryData = ref({});
 const route = useRoute();
@@ -43,6 +44,25 @@ onMounted(() => getBanner());
             <img :src="item.imgUrl" alt="" />
           </el-carousel-item>
         </el-carousel>
+      </div>
+    </div>
+    <div class="sub-list">
+      <h3>全部分类</h3>
+      <ul>
+        <li v-for="i in categoryData.children" :key="i.id">
+          <RouterLink to="/">
+            <img :src="i.picture" />
+            <p>{{ i.name }}</p>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
+    <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+      <div class="head">
+        <h3>- {{ item.name }}-</h3>
+      </div>
+      <div class="body">
+        <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
       </div>
     </div>
   </div>
@@ -133,7 +153,7 @@ onMounted(() => getBanner());
   //   position: absolute;
   left: 0;
   top: 0;
-//   z-index: 98;
+  //   z-index: 98;
 
   img {
     width: 100%;
