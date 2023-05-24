d2aece0b16ae5b405b5359f4b174826032f08b27
你的名字或昵称 <1584171644@qq.com>
2023-05-24 09:14:26 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,10 @@
 <script setup>
-import HomeCategory from './components/HomeCategory.vue'
-import HomeBanner from './components/HomeBanner.vue'
-import HomeNew from './components/HomeNew.vue'
-import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeHot from "./components/HomeHot.vue";
+import homeProduct from "./components/HomeProduct.vue";
+import HomePanelVue from "./components/HomePanel.vue";
 </script>
 
 <template>
@@ -14,4 +15,11 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <!-- 测试面板组件 -->
+  <HomePanelVue title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>我是新鲜好物的插槽</div>
+  </HomePanelVue>
+  <HomePanelVue title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>我是人气推荐的插槽</div>
+  </HomePanelVue>
 </template>
