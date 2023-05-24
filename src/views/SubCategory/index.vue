01c7712b975dc14acc87ec6ed410c07c1172f3b2
你的名字或昵称 <1584171644@qq.com>
2023-05-24 16:39:13 +0800

    二级分类-列表无线加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -26,6 +26,25 @@ const getGoodList = async () => {
   goodList.value = res.result.items;
 };
 onMounted(() => getGoodList());
+
+//tab切换回调
+const tabChange = () => {
+  reqData.value.page = 1;
+  getGoodList();
+};
+
+// 加载更多
+const disabled = ref(false);
+const load = async () => {
+  // 获取下一页的数据
+  reqData.value.page++;
+  const res = await getSubCategoryAPI(reqData.value);
+  goodList.value = [...goodList.value, ...res.result.items];
+  //   加载完毕 停止监听
+  if (res.result.items.length === 0) {
+    disabled.value = true;
+  }
+};
 </script>
 
 <template>
@@ -41,14 +60,22 @@ onMounted(() => getGoodList());
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div
+        class="body"
+        v-infinite-scroll="load"
+        :infinite-scrll-disabled="disabled"
+      >
         <!-- 商品列表-->
-        <GoodsItem v-for="good in goodList" :good="good" :key="good.id"></GoodsItem>
+        <GoodsItem
+          v-for="good in goodList"
+          :good="good"
+          :key="good.id"
+        ></GoodsItem>
       </div>
     </div>
   </div>
