3f25e0e03443de819cbab09e263415688458acd8
你的名字或昵称 <1584171644@qq.com>
2023-05-24 14:11:26 +0800

    一级分类-按照业务逻辑拆分函数

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,35 +1,10 @@
 <script setup>
-import { getCategoryAPI } from "@/apis/category";
-import { onMounted, ref } from "vue";
-import { onBeforeRouteUpdate, useRoute } from "vue-router";
-import { getBannerAPI } from "@/apis/home";
 import GoodsItem from "../Home/components/GoodsItem.vue";
-
-const categoryData = ref({});
-const route = useRoute();
-const getCategory = async (id = route.params.id) => {
-  const res = await getCategoryAPI(id);
-  categoryData.value = res.result;
-};
-onMounted(() => getCategory());
-
-// 期望路由参数变化的时候 可以把分类数据接口重新发送
-onBeforeRouteUpdate((to) => {
-  // 存在问题：使用最新的路由参数请求最新的分类数据
-  getCategory(to.params.id);
-});
+import { useBanner } from "./composables/useBanner";
+import { useCategory } from "./composables/useCategory";
+const { bannerList } = useBanner();
+const { categoryData } = useCategory();
 // 获取banner
-const bannerList = ref([]);
-const getBanner = async () => {
-  const res = await getBannerAPI({
-    distributionSite: "2",
-  });
-
-  bannerList.value = res.result;
-  //   console.log(res);
-};
-
-onMounted(() => getBanner());
 </script>
 
 <template>
