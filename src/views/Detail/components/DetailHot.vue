5a0512fbe44f00a5ca062fed6bbb17d55696c584
你的名字或昵称 <1584171644@qq.com>
2023-05-25 10:03:07 +0800

    详情-热榜title和列表内容适配

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -1,13 +1,24 @@
 <script setup>
 import { fetchHotGoodsAPI } from "@/apis/detail";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
+
+const props = defineProps({
+  hotType: { Number },
+});
+
+const TYPEMAP = {
+  1: "24小时热榜",
+  2: "周热榜",
+};
+
+const title = computed(() => TYPEMAP[props.hotType]);
 const hotList = ref([]);
 const route = useRoute();
 const getHotList = async () => {
   const res = await fetchHotGoodsAPI({
     id: route.params.id,
-    type: 1,
+    type: props.hotType,
   });
   hotList.value = res.result;
 };
@@ -17,7 +28,7 @@ onMounted(() => getHotList());
 
 <template>
   <div class="goods-hot">
-    <h3>周日榜单</h3>
+    <h3>{{ title }}</h3>
     <!-- 商品区块 -->
     <RouterLink
       to="/"
