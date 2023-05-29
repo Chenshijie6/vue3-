a38270ed956bf820f2519f2870debaafe8b352ab
你的名字或昵称 <1584171644@qq.com>
2023-05-29 14:48:26 +0800

    接口购物车-退出登录时清除购物车数据

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -54,10 +54,10 @@ export const useCartStore = defineStore('cart', () => {
 
     }
 
-    // // 清除购物车
-    // const clearCart = () => {
-    //     cartList.value = []
-    // }
+    // 清除购物车
+    const clearCart = () => {
+        cartList.value = []
+    }
 
     // 单选功能
     const singleCheck = (skuId, selected) => {
@@ -93,7 +93,7 @@ export const useCartStore = defineStore('cart', () => {
         isAll,
         selectedCount,
         selectedPrice,
-        // clearCart,
+        clearCart,
         addCart,
         delCart,
         singleCheck,
