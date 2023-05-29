41f5f92b6f21a44b776219ecacb6628634a4161b
你的名字或昵称 <1584171644@qq.com>
2023-05-29 16:33:08 +0800

    结算-提价订业务实现单

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { getCheckInfoAPI } from "@/apis/checkout";
 import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+import { createOrderAPI } from "@/apis/checkout";
+import { useCartStore } from "@/stores/cartStore";
+const cartStore = useCartStore();
+const router = useRouter();
 const checkInfo = ref({}); // 订单对象
 const curAddress = ref({});
 const getCheckInfo = async () => {
@@ -28,6 +33,31 @@ const confirm = () => {
   curAddress.value = activeAddress.value;
   showDialog.value = false;
   activeAddress.value = {};
+};
+
+// 创建订单
+const createOrder = async () => {
+  const res = await createOrderAPI({
+    deliveryTimeType: 1,
+    payType: 1,
+    payChannel: 1,
+    buyerMessage: "",
+    goods: checkInfo.value.goods.map((item) => {
+      return {
+        skuId: item.skuId,
+        count: item.count,
+      };
+    }),
+    addressId: curAddress.value.id,
+  });
+  const orderId = res.result.id;
+  router.push({
+    path: "/pay",
+    query: {
+      id: orderId,
+    },
+  });
+  cartStore.updateNewList(); //   更新购物车
 };
 </script>
 
@@ -138,7 +168,9 @@ const confirm = () => {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large">提交订单</el-button>
+          <el-button @click="createOrder" type="primary" size="large"
+            >提交订单</el-button
+          >
         </div>
       </div>
     </div>
