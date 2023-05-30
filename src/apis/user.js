1731157153a5c320d218d89afda9da51cb97ef49
你的名字或昵称 <1584171644@qq.com>
2023-05-30 13:35:42 +0800

    订单-使用基础参数获取订单列表

--- src/apis/user.js
+++ src/apis/user.js
@@ -19,3 +19,18 @@ export const getLikeListAPI = ({ limit = 4 }) => {
         }
     })
 }
+
+/*
+params: {
+    orderState:0,
+  page:1,
+  pageSize:2
+}
+*/
+export const getUserOrder = (params) => {
+    return request({
+        url: '/member/order',
+        method: 'GET',
+        params
+    })
+}
